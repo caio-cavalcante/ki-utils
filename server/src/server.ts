e28f302734738f79b8f7ca982d336101f98b94ab
caio <caio.cav.ara@gmail.com>
2025-12-28 19:44:25 -0300

    feat: use cors in backend server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,13 @@ import 'dotenv/config';
 import express from 'express'
 import type { Request, Response } from 'express'
 import journalRoutes from './routes/journal.routes'
+import cors from 'cors';
 
 const app = express();
 const PORT = 3333;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
     res.json({message: 'Hello World from the backend!'})
