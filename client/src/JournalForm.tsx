534ce1dc389d55d4b789fcd68f5ba547db04b390
caio <caio.cav.ara@gmail.com>
2026-01-11 16:08:57 -0300

    feat: enhance journal form layout and mood selection options

--- client/src/JournalForm.tsx
+++ client/src/JournalForm.tsx
@@ -17,24 +17,62 @@ const JournalForm = ({ onEntryCreated }: JournalFormProps) => {
     };
 
     return (
-        <div className="bg-gray-900 m-4 p-4 rounded-lg w-[90%]">
-            <form onSubmit={handleSubmit}>
-                <textarea name="content" id="content" placeholder="What did you do?"
-                className="bg-gray-700 rounded-lg p-2 w-[60%]"/>
-                <select name="mood" id="mood" className="bg-gray-700 rounded-lg p-2 w-[15%]">
-                    <option value="Happy">Happy</option>
-                    <option value="Sad">Sad</option>
-                    <option value="Afraid">Afraid</option>
-                    <option value="Disgusted">Disgusted</option>
-                    <option value="Angry">Angry</option>
-                    <option value="Anxious">Anxious</option>
-                    <option value="Jealous">Jealous</option>
-                    <option value="Embarrassed">Embarrassed</option>
-                    <option value="Bored">Bored</option>
-                    <option value="Surprised">Surprised</option>
-                    <option value="Tired">Tired</option>
+        <div className="bg-gray-200 dark:bg-gray-900 m-4 p-4 rounded-lg w-[60%]">
+            <form onSubmit={handleSubmit} className="flex items-center gap-3">
+                <textarea name="content" id="content" placeholder="What did you do?" rows={1} className="flex-1 resize-none rounded-md bg-gray-300 dark:bg-gray-700 rounded-lg px-3 py-2 w-[40%]"/>
+                
+                <select name="mood" id="mood" className="bg-gray-300 dark:bg-gray-700 rounded-lg px-3 py-2 w-[25%]">
+                    <option value="default" selected disabled>How did you feel?</option>
+                    <optgroup label="Happy">
+                        <option value="joyful">Joyful</option>
+                        <option value="content">Content</option>
+                        <option value="excited">Excited</option>
+                    </optgroup>
+
+                    <optgroup label="Loved">
+                        <option value="appreciated">Appreciated</option>
+                        <option value="valued">Valued</option>
+                        <option value="accepted">Accepted</option>
+                    </optgroup>
+
+                    <optgroup label="Confident">
+                        <option value="brave">Brave</option>
+                        <option value="hopeful">Hopeful</option>
+                        <option value="powerful">Powerful</option>
+                    </optgroup>
+
+                    <optgroup label="Playful">
+                        <option value="creative">Creative</option>
+                        <option value="curious">Curious</option>
+                        <option value="interested">Interested</option>
+                    </optgroup>
+
+                    <optgroup label="Sad">
+                        <option value="disappointed">Disappointed</option>
+                        <option value="hurt">Hurt</option>
+                        <option value="lonely">Lonely</option>
+                    </optgroup>
+
+                    <optgroup label="Scared">
+                        <option value="anxious">Anxious</option>
+                        <option value="worried">Worried</option>
+                        <option value="powerless">Powerless</option>
+                    </optgroup>
+
+                    <optgroup label="Angry">
+                        <option value="overwhelmed">Overwhelmed</option>
+                        <option value="frustrated">Frustrated</option>
+                        <option value="annoyed">Annoyed</option>
+                    </optgroup>
+
+                    <optgroup label="Embarrassed">
+                        <option value="ashamed">Ashamed</option>
+                        <option value="guilty">Guilty</option>
+                        <option value="excluded">Excluded</option>
+                    </optgroup>
                 </select>
-                <button type="submit" className="bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out cursor-pointer">Save</button>
+
+                <button type="submit" className="bg-green-400 hover:bg-green-700 text-white font-bold px-4 py-2 rounded transition duration-300 ease-in-out cursor-pointer">Save</button>
             </form>
         </div>
     )
