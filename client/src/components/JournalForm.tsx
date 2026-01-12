040f525788755cc7fe7556e1caf2164bb69c4cd6
caio <caio.cav.ara@gmail.com>
2026-01-11 22:25:35 -0300

    feat: add JournalForm to components and receive everything via props

--- client/src/components/JournalForm.tsx
+++ client/src/components/JournalForm.tsx
@@ -1,43 +1,44 @@
 import { useState } from "react";
-import { journalService } from "./services/journalService";
+import { journalService } from "../services/journalService";
 
 interface JournalFormProps {
-    onEntryCreated: () => void;
+    content: string;
+    mood: string;
+    setContent: (value: string) => void;
+    setMood: (value: string) => void;
+    onSubmit: (e: React.FormEvent) => void;
+    isEditing: boolean; // Para mudar o texto do botão
+    onCancelEdit: () => void; // Para cancelar a edição
 }
 
-const JournalForm = ({ onEntryCreated }: JournalFormProps) => {
-    const [selectedMood, setSelectedMood] = useState("");
-
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const form = event.currentTarget;
-        const formData = new FormData(form);
-        const content = formData.get("content") as string;
-        const mood = formData.get("mood") as string;
-
-        await journalService.create(content, mood);
-        onEntryCreated();
-        form.reset();
-        setSelectedMood("");
-    };
-
+const JournalForm = ({
+    content,
+    mood,
+    setContent,
+    setMood,
+    onSubmit,
+    isEditing,
+    onCancelEdit,
+}: JournalFormProps) => {
     return (
         <div className="bg-gray-200 dark:bg-gray-900 m-4 p-4 rounded-lg w-[60%]">
-            <form onSubmit={handleSubmit} className="flex items-center gap-3">
+            <form onSubmit={onSubmit} className="flex items-center gap-3">
                 <textarea
                     name="content"
                     id="content"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
                     placeholder="What did you do?"
-                    rows={1}
                     className="flex-1 resize-none rounded-md bg-gray-300 dark:bg-gray-700 rounded-lg px-3 py-2 w-[40%]"
+                    rows={1}
                 />
 
                 <select
                     name="mood"
                     id="mood"
+                    value={mood}
+                    onChange={(e) => setMood(e.target.value)}
                     className="bg-gray-300 dark:bg-gray-700 rounded-lg px-3 py-2 w-[25%]"
-                    value={selectedMood}
-                    onChange={(e) => setSelectedMood(e.target.value)}
                 >
                     <option value="" disabled>
                         How did you feel?
@@ -95,8 +96,18 @@ const JournalForm = ({ onEntryCreated }: JournalFormProps) => {
                     type="submit"
                     className="bg-green-400 hover:bg-green-700 text-white font-bold px-4 py-2 rounded transition duration-300 ease-in-out cursor-pointer"
                 >
-                    Save
+                    {isEditing ? "Update" : "Add"}
                 </button>
+
+                {isEditing && (
+                    <button
+                        type="button"
+                        onClick={onCancelEdit}
+                        className="bg-red-400 hover:bg-red-700 text-white font-bold px-4 py-2 rounded transition duration-300 ease-in-out cursor-pointer"
+                    >
+                        Cancel
+                    </button>
+                )}
             </form>
         </div>
     );
